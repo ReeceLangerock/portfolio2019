fecfbb41e7d32ce05e486b52ca028a0b40ff8f6b
reecelangerock <reecelangerock@gmail.com>
2019-02-09 15:20:34 -0600

    added some portfolio content and got the individual item working and looking as desired

--- src/components/portfolio/PortfolioContainer.js
+++ src/components/portfolio/PortfolioContainer.js
@@ -10,19 +10,107 @@ class PortfolioContainer extends Component {
     const items = [
       {
         id: 1,
+        title: 'DeepDive.ML',
+        synopsis:
+          'DeepDive is a site to supplement the Madden Online CFM experience. The game itself does not offer advanced statistics, player rankings, fantasy data or automatically generated user content. DeepDive had many of the intended features (and some the beginnings of some new ones) in place I ultimately lost interest in Madden and abandoned the project. I am in the process of open-sourcing the code and looking for someone to take over the project.',
+        tech: [
+          'React',
+          'Redux',
+          'Semantic UI',
+          'Express.js',
+          'MongoDB',
+          'Socket.io',
+          'Victory Charts',
+        ],
+        private: true,
+        date: 'August 2018',
+        siteLink: 'https://m538.herokuapp.com/revo',
+        repoLink: '',
+      },
+      {
+        id: 2,
+        title: 'CFMScout',
+        synopsis:
+          'CFMScout is a Companion App for the Madden NFL Video Game. It is built in React-Native and is available for download on the Google Play Store. CFMScout allows users to add their league and prospect combine data, the app will then calculate estimates for speed, acceleration, agility, strength and jumping attributes, along with a SPARQ score. Users can also see how the prospects combine scores compare with prospects entered by other users',
+        tech: [
+          'React Native',
+          'Redux',
+          'MongoDB',
+          'MongoDB Stitch',
+          'Styled-Components',
+          'AdMob',
+        ],
+        private: true,
+        date: 'August 2018',
+        siteLink:
+          'https://play.google.com/store/apps/details?id=com.cfmscout&hl=en_US',
+      },
+      {
+        id: 3,
         title: 'Draw-Dash',
         synopsis:
-          'Draw-Dash is a competitive, 1v1 drawing competition. Authenticated users are given a prompt for what to draw and a timer begins to countdown, and the 60 second Draw Dash begins!',
+          'Draw-Dash is a competitive, 1v1 drawing competition. Authenticated users are given a prompt for what to draw, a timer begins to countdown and the 60 second Draw Dash begins!',
         tech: ['React', 'React-Redux', 'Express', 'Socket.io', 'MongoDB'],
         repoLink: 'https://github.com/ReeceLangerock/draw-dash',
         siteLink: 'https://draw-dash.herokuapp.com/',
+        private: false,
+        date: '2017',
+        images: [
+          'https://reecelangerock.ml/static/media/Draw-Dash.f3ed75ba.png',
+          'https://reecelangerock.ml/static/media/Draw-Dash-Preview.24c2f723.png',
+        ],
       },
       {
-        id: 17,
-        title: 'test2',
-        synopsis: 'sdjfndsfjn',
-        tech: ['React'],
+        id: 4,
+        title: 'Mansion Men',
+        synopsis:
+          'This website was built for a long-running fantasy football league that I am a member of. It provides visualization of user and league statistical data, tracking of league trophy records, and reference for league rules and previous draft results',
+        tech: ['React', 'Redux', 'Create-React-App', 'D3', 'Victory Charts'],
+        private: false,
+        date: '2017',
+
+        siteLink: 'http://mansion-men.surge.sh/',
+        repoLink: 'https://github.com/ReeceLangerock/MMFantasyLeague',
       },
+      {
+        id: 5,
+        title: 'Libru',
+        synopsis:
+          'Libru is a programming resource library built to allow self-taught developers to find, rate and review resources as well as track what courses, books, guides, etc they have completed, are working on, or want to do.',
+        tech: ['React', 'Redux', 'Create-React-App', 'D3', 'Victory Charts'],
+        private: false,
+        date: '2017',
+
+        siteLink: 'http://mansion-men.surge.sh/',
+        repoLink: 'https://github.com/ReeceLangerock/MMFantasyLeague',
+      },
+      {
+        id: 6,
+        title: 'Game of Life',
+        synopsis: `This was built as a part of the FreeCodeCamp data visualization certification course. The goal was to build a React application that is functionally similar to`,
+        tech: [
+          'React',
+          'Create-React-App',
+          'Styled-Components',
+          'Jest',
+          'Travis CI',
+        ],
+        private: false,
+        date: '2017',
+        fcc: 'https://codepen.io/FreeCodeCamp/full/reGdqx/',
+        siteLink: 'http://game-of-life-rl.surge.sh/',
+        repoLink: 'https://github.com/ReeceLangerock/game-of-life',
+      },
+      // {
+      //   id: 0,
+      //   title: '',
+      //   synopsis: '',
+      //   tech: [],
+      //   private: false,
+      //   date: '',
+      //   siteLink: '',
+      //   repoLink: '',
+      // },
     ]
 
     return items.map((item, index) => {
--- src/components/portfolio/PortfolioItem.js
+++ src/components/portfolio/PortfolioItem.js
@@ -2,10 +2,28 @@ import React, { Component } from 'react'
 import styled from 'styled-components'
 
 class BlogItem extends Component {
-  renderTech() {}
+  renderImages() {
+    const { images } = this.props.item
+    console.log(images)
+    if (!images) {
+      return null
+    }
+    return images.map(image => {
+      return (
+        <ImageContainer>
+          <img src={image} />
+        </ImageContainer>
+      )
+    })
+  }
   render() {
     const { active, item, onSelectItem } = this.props
     const technologies = item.tech.join(', ')
+
+    if(item.fcc){
+      item.synopsis = <span>{item.synopsis} <a href = {item.fcc} target = '_blank'>this</a></span>
+    }
+
     return (
       <ItemContainer active={active}>
         <ItemTitle active={active} onClick={e => onSelectItem(item.id)}>
@@ -13,17 +31,34 @@ class BlogItem extends Component {
           {active && <Toggle>X</Toggle>}
         </ItemTitle>
         <ItemContent active={active}>
-          <p>SYNOPSIS: {item.synopsis}</p>
-          <p>TECHNOLOGIES: {technologies}</p>
+          <p>
+            <Label>SYNOPSIS: </Label>
+            {item.synopsis}
+          </p>
+          <p>
+            <Label>TECHNOLOGIES: </Label>
+            {technologies}
+          </p>
 
-          <Link href={item.repoLink}>LINK TO REPO</Link>
+          <p>
+            <Label>DATE COMPLETED: </Label>
+            {item.date}
+          </p>
 
-          <Link href={item.siteLink}>LINK TO SITE</Link>
+          <Link href={item.siteLink} target = '_blank'>LINK TO SITE</Link>
+          <br/>
+          {(item.repoLink || !item.private) && (
+            <Link href={item.repoLink} target = '_blank'>LINK TO REPO</Link>
+          )}
 
-          <Row>
-            <img src="https://reecelangerock.ml/static/media/Draw-Dash-Preview.24c2f723.png" />
-            <img src="https://reecelangerock.ml/static/media/Draw-Dash.f3ed75ba.png" />
-          </Row>
+          {item.private && (
+            <p>
+              This project is in a private repository. I am more than happy to
+              share it upon request.
+            </p>
+          )}
+
+          <Row>{this.renderImages()}</Row>
         </ItemContent>
       </ItemContainer>
     )
@@ -34,13 +69,13 @@ export default BlogItem
 
 const ItemContainer = styled.div`
   border: 1px solid white;
-  font-size: 32px;
+  font-size: 36px;
   background: ${props =>
     props.active ? 'hsla(154, 95%, 24%, 1)' : 'hsla(154, 95%, 24%, 0.5)'};
 `
 const ItemTitle = styled.div`
   border-bottom: ${props => (props.active ? '1px solid white' : 'none')};
-  padding: 6px 10px;
+  padding: 10px 10px;
   display: flex;
   justify-content: space-between;
   cursor: pointer;
@@ -49,29 +84,52 @@ const ItemContent = styled.div`
   display: ${props => (props.active ? 'block' : 'none')};
   padding: 10px;
   font-size: 28px;
+  p {
+    margin-bottom: 12px;
+  }
+  a {
+    color: white;
+    transition: 0.35s all;
+
+  &:hover {
+    text-decoration: underline;
+    color: #21d077;
+    transition: 0.35s all;
+  }
+  }
+`
+
+const Label = styled.span`
+  font-size: 28px;
+  margin-right: 4px;
 `
 
 const Row = styled.div`
   display: flex;
-  justify-content: space-between;
-  margin-bottom: 10px;
+  justify-content: space-around;
+  position: relative;
   flex-wrap: wrap;
+  padding: 15px 8px 0 8px;
   align-items: center;
+`
+
+const ImageContainer = styled.div`
+  display: block;
 
   img {
-    max-height: 400px;
-    border: 3px solid #020701;
-    border-radius: 0.5em;
-    padding: 3px;
-    background: #041704;
+    max-height: 250px;
+    color: #031603;
+    box-shadow: 0 -4px #dbd8ff, 0 -8px, 4px 0 #dbd8ff, 4px -4px, 8px 0,
+      0 4px #dbd8ff, 0 8px, -4px 0 #dbd8ff, -4px 4px, -8px 0, -4px -4px, 4px 4px;
+    background: #dbd8ff;
+    padding: 1px;
   }
 `
 const Toggle = styled.div``
 
 const Link = styled.a`
-  display: block;
   margin-bottom: 10px;
-  font-size: 32px;
+  font-size: 28px;
   text-decoration: none;
   color: white;
   transition: 0.35s all;
